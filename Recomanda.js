e162f2ce10ddf325a95d6c750b147f7f909edd1b
! Cata <107345980+CataJudah@users.noreply.github.com>
2025-07-05 13:29:32 +0300

    translate recomanda

--- Recomanda.js
+++ Recomanda.js
@@ -13,3 +13,44 @@ document.getElementById("toggleButton").addEventListener("click", function () {
       icon.classList.add("fa-bars"); // Adaugă iconița meniului închis
   }
 });
+
+const roFlag = document.getElementById('roFlag');
+        const enFlag = document.getElementById('enFlag');
+        let currentLanguage = 'ro';
+
+        function translatePage(targetLang) {
+            const elementsToTranslate = document.querySelectorAll('[data-ro], [data-en]');
+
+            if (targetLang === 'en') {
+                elementsToTranslate.forEach(element => {
+                    if (element.dataset.en !== undefined) {
+                        element.textContent = element.dataset.en;
+                    }
+                });
+                document.documentElement.lang = 'en';
+                currentLanguage = 'en';
+                enFlag.style.display = 'none';
+                roFlag.style.display = 'inline-block';
+            } else {
+                elementsToTranslate.forEach(element => {
+                    if (element.dataset.ro !== undefined) {
+                        element.textContent = element.dataset.ro;
+                    }
+                });
+                document.documentElement.lang = 'ro';
+                currentLanguage = 'ro';
+                enFlag.style.display = 'inline-block';
+                roFlag.style.display = 'none';
+            }
+        }
+
+        enFlag.addEventListener('click', () => translatePage('en'));
+        roFlag.addEventListener('click', () => translatePage('ro'));
+
+        if (currentLanguage === 'ro') {
+            enFlag.style.display = 'inline-block';
+            roFlag.style.display = 'none';
+        } else {
+            enFlag.style.display = 'none';
+            roFlag.style.display = 'inline-block';
+        }
