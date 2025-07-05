d44979f1efb9ce83dd03f6bc7f16c75efba711cd
! Cata <107345980+CataJudah@users.noreply.github.com>
2025-07-05 13:24:11 +0300

    dd

--- MainPage.js
+++ MainPage.js
@@ -67,3 +67,45 @@ scrollContainers.forEach((container) => {
 	// 	article.addEventListener("mouseleave", resumeScrolling);
 	// }
 });
+
+
+const roFlag = document.getElementById('roFlag');
+	const enFlag = document.getElementById('enFlag');
+	let currentLanguage = 'ro';
+
+	function translatePage(targetLang) {
+		const elementsToTranslate = document.querySelectorAll('[data-ro], [data-en]');
+
+		if (targetLang === 'en') {
+			elementsToTranslate.forEach(element => {
+				if (element.dataset.en !== undefined) {
+					element.textContent = element.dataset.en;
+				}
+			});
+			document.documentElement.lang = 'en';
+			currentLanguage = 'en';
+			enFlag.style.display = 'none';
+			roFlag.style.display = 'inline-block';
+		} else {
+			elementsToTranslate.forEach(element => {
+				if (element.dataset.ro !== undefined) {
+					element.textContent = element.dataset.ro;
+				}
+			});
+			document.documentElement.lang = 'ro';
+			currentLanguage = 'ro';
+			enFlag.style.display = 'inline-block';
+			roFlag.style.display = 'none';
+		}
+	}
+
+	enFlag.addEventListener('click', () => translatePage('en'));
+	roFlag.addEventListener('click', () => translatePage('ro'));
+
+	if (currentLanguage === 'ro') {
+		enFlag.style.display = 'inline-block';
+		roFlag.style.display = 'none';
+	} else {
+		enFlag.style.display = 'none';
+		roFlag.style.display = 'inline-block';
+}
